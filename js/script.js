894eacffa02e82001add3067ea6c17e4e8924594
Oskar Pozniak <oskpozn@gmail.com>
2019-10-06 09:17:23 +0200

    dodano else...if dla komputera

--- js/script.js
+++ js/script.js
@@ -12,3 +12,10 @@ if(playerInput == '1'){
     playerMove = "nożyce"
 }
 
+if (randomNumber == 1) {
+    computerMove = "kamień"
+} else if (randomNumber == 2) {
+    computerMove = "papier"
+} else {
+    computerMove = "nożyce"
+}
